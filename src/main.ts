0a77fdc319f35a132f00398dbc6eed53ecffa342
lpchi133 <lpchi21@clc.fitus.edu.vn>
2024-11-05 16:50:58 +0700

    fix

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: process.env.FRONTEND_URL,
+    origin: process.env.FRONTEND_URL ?? 'http://localhost:5173',
     credentials: true,
   });
-  await app.listen(3000);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
